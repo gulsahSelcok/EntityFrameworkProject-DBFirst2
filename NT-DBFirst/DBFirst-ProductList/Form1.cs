fbdde8c6bebd64f010b12f6af7711923f2dd7de8
gulsahSelcok <49310029+gulsahSelcok@users.noreply.github.com>
2019-07-18 01:07:43 +0300

    Add files via upload

---
+++ NT-DBFirst/DBFirst-ProductList/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBFirst_ProductList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //productıd productname category name
+        NorthwindEntities db = new NorthwindEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var obj = db.Products.Select(x => new {
+                productId = x.ProductID,
+                productName = x.ProductName,
+                categoryName = x.Category.CategoryName
+            }).ToList(); // var yerine IEnumerable yazılabilir. yada IList
+
+            dataGridView1.DataSource = obj;
+        }
+    }
+}
